902b9d432641c2b2684865fb63f7cdddc7e6aba8
BuschMath <busch@iowacentral.edu>
2022-09-12 10:23:07 -0500

    Division

--- Ch2_Example_FA22/main.cpp
+++ Ch2_Example_FA22/main.cpp
@@ -2,41 +2,14 @@
 
 using namespace std;
 
-int Add(int a, int b);
-
 int main()
 {
-	char test = 'A';
-	char test2 = '!';
+	int iNum = 17;
+	int iNum2 = 5;
+	float fNum = 17;
+	float fNum2 = 7.1;
 
-	int num = test + 32;
-	unsigned int large = 2147483647 + 1;
-
-	cout << large;
-
-	cout << endl;
-
-	float val = 1;
-
-	float compare = 0;
-
-	compare = 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f;
-
-	cout << val << " is the same as " << compare;
-
-	if (val == compare)
-	{
-		cout << "\n1 is equal to 1.";
-	}
-	else
-	{
-		cout << "\n1 is not equal to 1.";
-	}
+	cout << int(fNum / iNum2) << endl << iNum % iNum2;
 
 	return 0;
-}
-
-int Add(int a, int b)
-{
-	return a + b;
 }
